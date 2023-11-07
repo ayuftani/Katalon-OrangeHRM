70c1880aac35430b21dd5c634e18aae72d4c1126
51206151 <51206151@NBLV18100002.adira.co.id>
2023-11-07 14:41:26 +0700

    Update skenario verifikasi login berhasil

---
+++ Object Repository/Login/label_dashboard.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_dashboard</name>
+   <tag></tag>
+   <elementGuidId>d08217d4-5ad4-4299-ae78-7ece3980a2cb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[text()='Dashboard']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
